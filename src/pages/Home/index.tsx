55ec8365e3b42240647a02909b7c6d063207e202
tooooo1 <wer4272@nate.com>
2022-04-05 14:47:21 +0900

    feat : Home Page 추가

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,22 @@
+import { Positioner } from '../../components/Wrapper/styled';
+import * as Styled from './styled';
+import { useNavigate } from 'react-router-dom';
+
 const Home = () => {
-  return <></>;
+  let navigate = useNavigate();
+
+  return (
+    <Positioner>
+      <Styled.GlobalStyle />
+      <Styled.Title>다른 색깔 찾기</Styled.Title>
+      <Styled.Button id="list" bgcolor="#ff2e35" onClick={() => navigate('/play')}>
+        게임시작
+      </Styled.Button>
+      <Styled.Button bgcolor="#01a8ff" onClick={() => navigate('/rank')}>
+        랭킹보기
+      </Styled.Button>
+    </Positioner>
+  );
 };
 
 export default Home;
--- src/pages/Home/styled.ts
+++ src/pages/Home/styled.ts
@@ -1,1 +1,35 @@
-// import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #f9f9f9;
+  }
+`;
+
+export const Title = styled.div`
+  font-size: 8vh;
+  margin-bottom: 2rem;
+  text-align: center;
+  font-family: 'RixYeoljeongdo_Regular';
+  word-break: keep-all;
+`;
+
+export const Button = styled.button<{ bgcolor: string }>`
+  width: 100%;
+  padding: 1rem 0;
+  background-color: ${(props) => props.bgcolor};
+  border-radius: 7px;
+  border: none;
+  color: white;
+  font-weight: bold;
+  font-size: 1.5rem;
+  transition: 0.3s all ease;
+  font-family: 'Pretendard-Bold';
+  cursor: pointer;
+  :hover {
+    transform: scale(1.1);
+  }
+  &#list {
+    margin: 10px 0;
+  }
+`;
